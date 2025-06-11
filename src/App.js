2b426a6372573040ac91ca484fe3ee6a75912abe
vidhusan12 <vidhusan709@gmail.com>
2025-06-11 22:26:26 +1200

    Added SearchBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import { useState } from "react";
+import SearchBar from "./components/SearchBar";
+
+
 
 function App() {
-  return <h1>MovieSearch App</h1>;
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleSearchChange(newTerm) {
+    setSearchTerm(newTerm);
+  }
+
+  function handleSearchSubmit(e) {
+    e.preventDefault();
+    console.log("Searching for: ", searchTerm);
+  }
+  return (
+    <>
+      <h1>MovieSearch App</h1>
+      <SearchBar
+        value={searchTerm}
+        onChange={handleSearchChange}
+        onSubmit={handleSearchSubmit}
+      />
+    </>
+
+  )
 }
 
 export default App;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SearchBar({ value, onChange, onSubmit }) {
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" value={value} onChange={(e) => onChange(e.target.value)} />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+export default SearchBar;
